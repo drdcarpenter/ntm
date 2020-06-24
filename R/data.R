f193fad1b69122eb3a637ca2c2818b40e7403446
Dan Carpenter <drdcarpenter@googlemail.com>
2020-06-24 21:11:07 +0100

    add data to package

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' NAMM, PONS, Lrr and Lat values for 5 sites
+#'
+#' A dataset containing NAMM, PONS, Lrr and Lat values for five sites, with and
+#' without aircraft noise included.  These data are taken from page 95 of 
+#' Bentley (2019) Tranquil Spaces. Sharps Redmore Press.
+#'
+#' @format A data frame with 10 rows and 6 variables:
+#' \describe{
+#'   \item{NAMM}{NAMM score, from 1 -5}
+#'   \item{PONS}{PONS score, percentage natural sound}
+#'   \item{Lrr}{Lrr, road and rail noise in decibels}
+#'   \item{Lat}{Lat, corrected overal measure in decibels}
+#'   ...
+#' }
+#'
+"ntm"
